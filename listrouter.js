98aee0769236dfa43a7fc2c852eff790e6e89b71
Pierre <pierre.racine@sbf.ulaval.ca>
2021-07-20 14:09:27 -0400

    Move detailed API specs

--- listrouter.js
+++ listrouter.js
@@ -68,27 +68,64 @@ listRouter.delete('', function(req, res, next){
 
 module.exports = listRouter;
 
-/* Lists API
+/*
 
-  GET /api/lists             // Get all lists
-  GET /api/lists/:ownerid    // Get lists owned by a specific user
-  GET /api/list/:listid      // Get by id (might not be necessary since id can be passed as a parameter to GET /api/lists/:ownerid)
+View API
 
-  POST /api/lists            // post a new list
+  GET /api/view               // Get all views the users has permission to view
+                              // Status: 200, 401, 403, 404 (no views weres found)
+                              // Details: includes all views owned by the user
+                              // What if the user does not have permission to view the liked list
 
-  PUT /api/lists/:listid     // replace a list by id
+  GET /api/view/:ownerid      // Get all views owned by a specific user
+                              // Status: 200, 400 invalid userid, 401, 403(?), 404 (the requested userid was not found)
 
-  PATCH /api/lists/:listid   // patch a list by id
+  GET /api/view/:subs:userid  // Get all views a specific user has subscribed to
+                              // Status: 200, 400 invalid userid, 401, 403(?), 404 (the requested userid was not found)
 
-  DELETE /api/lists/:listid  // delete a list by id
+  GET /api/view/:viewid       // Get a view by viewid with all the linked list items
+                              // Status: 200, 401, 403, 404 (no view was found)
+
+  POST /api/view          // Post a new view and create a default linked list
+                          // Status: 201, 400 invalid json, 401, 403
+
+  PUT /api/view/:viewid   // Replace a view by id (and delete or create the linked list if necessary)
+                          // Status: 200, 400 invalid json or invalid viewid, 401, 403)
+
+  PATCH /api/view/:viewid // Patch a view by id (and delete or create the linked list if necessary)
+                          // Status: 200, 400 invalid json or invalid viewid, 401, 403)
+
+  DELETE /api/view/:viewid  // Delete a view by id and the linked data if no more view links to it
+                            // Status: 201, 400 invalid or invalid viewid, 401, 403)
+
+List API
+
+  GET /api/list/:listid   // Get a list and the linked listitems by listid
+                          // Status: 200, 400 invalid listid, 401, 403, 404 (the requested listid was not found)
+
+  POST /api/list          // Post a new list
+                          // Status: 201, 400 invalid json, 401, 403
+
+  PUT /api/list/:listid   // Replace a list by id
+                          // Status: 200, 400 invalid json or invalid listid, 401, 403)
+
+  PATCH /api/list/:listid // Patch a list by id (200, 400 invalid json or invalid listid, 401, 403)
+
+  DELETE /api/list/:listid  // delete a list by id (201, 400 invalid or invalid listid, 401, 403)
+
+Listitem API
+
+  POST /api/listitem            // Post one or many new list (201, 400 invalid json, 401, 403)
 
   Response status codes
 
+  - 200 OK
+  - 201 Created - posted resource have been created
   - 400 Bad Request – client sent an invalid request, such as lacking required request body or parameter
   - 401 Unauthorized – client failed to authenticate with the server
   - 403 Forbidden – client authenticated but does not have permission to access the requested resource
   - 404 Not Found – the requested resource does not exist
-  - 412 Precondition Failed – one or more conditions in the request header fields evaluated to false
+  - 412 Precondition Failed – one or more conditions in the request  fields evaluated to false
   - 422 Unprocessable Entity - Well formed but unprocessable request (not used)
   - 500 Internal Server Error – a generic error occurred on the server
   - 503 Service Unavailable – the requested service is not available
