d16e1705e04e3b298a289a58221a26be3f64c9b5
Pierre <pierre.racine@sbf.ulaval.ca>
2021-08-06 15:51:33 -0400

    First implementation of validation of JSON string against the schema

--- listItemSchema.js
+++ listItemSchema.js
@@ -1,11 +1,12 @@
 //var assert = require('assert');
 
+const { createMochaInstanceAlreadyRunningError } = require('mocha/lib/errors');
 const Utils = require('./utils/utils');
 
 class ItemSchema {
   constructor(schema) {
     if (schema == null) {
-      throw new Error('ItemSchema can not be null...');
+      throw new Error('ItemSchema: Schema can not be null...');
     }
     if (typeof schema === 'string') {
       this.schema = Utils.OTSchemaToJSON(schema);
@@ -15,42 +16,91 @@ class ItemSchema {
     }
 
     if (typeof this.schema !== 'object') {
-      throw new Error('ItemSchema: Passed schema is not a valid object...');
+      throw new Error('ItemSchema: JSON schema is not well formed...');
     }
+    this.requiredFields = [];
+    var self = this;
     this.validate();
   };
 
+  // validate this.schema properties
+  validateProperties(key, obj, parentKey) {
+    const validProperties = ['type', 'required']
+    if (!(validProperties.includes(key))) {
+      throw new Error('ItemSchema: Invalid property (' + key + ') for ' + parentKey + '...');
+    }
+    if (key == 'required') {
+      this.requiredFields.push(parentKey);
+    }
+  }
+
+  // validate passed JSON fields
+  validateField(key, obj, parentKey) {
+    if (!(Object.keys(this.schema).includes(key))) {
+      throw new Error('ItemSchema: JSON object is not valid. "' + key + '" is not a valid field for this schema...');
+    };
+    // iterate over each shema property for that field and call the corresponding validator
+    for (var property in this.schema[key]) {
+      console.log(property + ': ' + this.schema[key][property]);
+      if (property != 'required') {
+        var validatorName = 'validate_' +  this.schema[key][property];
+        obj[key] = this[validatorName](key, obj[key]); // pass object by value so the validator can modify it directly
+      }
+    }
+  }
+
+  validate_string(key, val) {
+    if (typeof val !== 'string') {
+      throw new Error('ItemSchema: JSON object is not valid. Field "' + key + '" value (' + val + ') is not a string...');
+    }
+    return val;
+  }
+
   // validate that this schema is valid
   validate() {
-    this.traverse(this.schema);
+    this.traverse(this.schema, null, 0, [null, this.validateProperties.bind(this), null]);
     return true;
   };
 
-  // validate a json strin against this schema
+  // validate a json string against this schema
   validateJson(jsonstr) {
-    // 1) validate required fields
+    // make sure passed json is valid json
+    const json = JSON.parse(jsonstr);
+
+    // 1) validate all required fields are present
+    const jsonkeys = Object.keys(json);
+    console.log(jsonkeys);
+    var missingField = '';
+    if (!(jsonkeys === null) && 
+          !(jsonkeys.length === 0) && 
+          !(this.requiredFields.every(field => {var inc = jsonkeys.includes(field); if (!inc) {missingField = field}; return inc}))) {
+      throw new Error('ItemSchema: JSON object is not valid. "' + missingField + '" is missing...');
+    }
+
     // 2) if strict invalidate non schema fields
     // 3) validate and sanitize each field
-    return true;
+    this.traverse(json, null, 0, [this.validateField.bind(this), null, null]);
+    return json;
   };
 
-  func(key, value, level) {
-    console.log(' '.repeat(2 * (level - 1)) + key + " : " + JSON.stringify(value));
-  }
+  traverse(obj, parentKey = null, level = 0, callbacks = null) {
+    for (var key in obj) {
+      console.log(' '.repeat(2 * (level)) + key + " : " + JSON.stringify(obj[key]));
+      
+      if (level == 3) {
+        throw new Error('Too many levels for ItemSchema "' + this.schema + '"...');
+      }
+      else if (!(callbacks[level] === null) && typeof callbacks[level] === 'function') {
+        callbacks[level](key, obj, parentKey);
+      }
 
-  traverse(obj, level = 1) {
-    if (level == 4) {
-      throw new Error('Too many level for schema ' + this.schema);
-    }
-    for (var i in obj) {
-        this.func(i, obj[i], level);  
-        if (obj[i] !== null && typeof(obj[i]) == "object") {
-            //going one step down in the object tree!!
-            this.traverse(obj[i], ++level);
-            level--;
-        }
-    }
-  }
-}
+      if (obj[key] !== null && typeof(obj[key]) == "object") {
+          //going one step down in the object tree!!
+          this.traverse(obj[key], key, ++level, callbacks);
+          level--;
+      };
+    };
+  };
+};
 
 module.exports = ItemSchema;
--- test/testListItemSchema.js
+++ test/testListItemSchema.js
@@ -8,7 +8,7 @@ var expect = chai.expect;
 
 describe('List Item Schema', () => {
   it('Pass null', () => {
-    expect(function(){new ItemSchema();}).to.throw('ItemSchema can not be null...');
+    expect(function(){new ItemSchema();}).to.throw('ItemSchema: Schema can not be null...');
   });
 
   it('Pass invalid schema string', () => {
@@ -18,13 +18,31 @@ describe('List Item Schema', () => {
   it('Pass a string schema', () => {
     var schema = new ItemSchema('field1: {type: string, required}');
     
-    expect(schema.validate('{field1: \'toto\'}')).to.be.true;
+    expect(schema).to.be.an('object');
   });
 
   it('Pass JSON schema', () => {
-    var schema = new ItemSchema(Utils.OTSchemaToJSON('{field1: {string: true, required: true}}'));
+    var schema = new ItemSchema(Utils.OTSchemaToJSON('{field1: {type:string, required}}'));
     
-    expect(schema.validate('{field1: \'toto\'}')).to.be.true;
+    expect(schema).to.be.an('object');
+  });
+
+  it('Validate a simple json string against a simple schema', () => {
+    var schema = new ItemSchema('field1: {type: string, required}');
+    
+    expect(schema.validateJson('{"field1": "toto"}')).to.be.an('object');
+  });
+
+  it('Validate a missing field', () => {
+    var schema = new ItemSchema('field1: {type: string, required}');
+    
+    expect(function(){schema.validateJson('{"field": "toto"}')}).to.throw('ItemSchema: JSON object is not valid. "field1" is missing...');
+  });
+
+  it('Invalid type (number instead of string)', () => {
+    var schema = new ItemSchema(Utils.OTSchemaToJSON('{field1: {type:string, required}}'));
+    
+    expect(function(){schema.validateJson('{"field1": 123}')}).to.throw('ItemSchema: JSON object is not valid. Field "field1" value (123) is not a string...');
   });
 });
 
