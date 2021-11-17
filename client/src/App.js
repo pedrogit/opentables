b58e2826c8d82c7f7974fcf6694bc5cb3e910961
Pierre <pierre.racine@sbf.ulaval.ca>
2021-11-17 15:23:59 -0500

    Set individual properties in the component

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,12 @@ import Box from '@mui/material/Box';
 import List from './List';
 
 function App() {
-  const [listData, setData] = React.useState(null);
+  const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
     fetch('http://localhost:3000/api/opentables/000000000000000000000005')
       .then(response => response.json())
-      .then(data => {
-        return setData({
-          template: data.item_template, 
-          schema: data._childlist.listschema,
-          items: data._childlist.items
-        });
-      });
+      .then(data => setData(data));
     }, []);
 
   console.log('App rendered');
@@ -26,7 +20,10 @@ function App() {
       alignItems: 'center',
       justifyContent: 'center'}}>
       {<Box sx={{ display: 'flex'}}>
-        {listData ? <List {...listData}/> : <CircularProgress />}
+        {data ? <List template = {data.item_template} 
+                      schema = {data._childlist.listschema}
+                      items = {data._childlist.items}
+                      /> : <CircularProgress />}
       </Box>}
     </div>
   );
