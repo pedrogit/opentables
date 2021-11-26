996e0d4cae1c0247c1cf0b4337a16809c2932413
Pierre <pierre.racine@sbf.ulaval.ca>
2021-11-25 19:12:56 -0500

    Popover when double clicking the component value

--- client/src/components.js
+++ client/src/components.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Popover from '@mui/material/Popover';
+import TextField from '@mui/material/TextField';
 
 function labelVal(props) {
   var fs = props.vertical ? '0.8em' : null;
@@ -12,23 +15,60 @@ function Label(props) {
   var defaultSx = {color: 'red', fontWeight: 'bold', marginRight: 1};
   return (
     <>
-      <Box sx={{...defaultSx, ...props.sx}}>
+      <Typography sx={{...defaultSx, ...props.sx}}>
         {props.val}{props.separator ? <> :</> : null}
-      </Box>
+      </Typography>
     </>
   );
 };
 
 function Text(props) {
   var defaultSx = {};
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  const editLabel = () => 'Edit the "' +  props.val.prop + '" property...';
+  const getWidth = () => 8*Math.max(props.val.val.length, editLabel().length);
+
   return (
     <>
       <Stack direction={props.vertical ? 'column' : 'row'}>
         {labelVal(props)}
-        <Box sx={{...defaultSx, ...props.sx}}>
+        <Typography sx={{...defaultSx, ...props.sx}} onDoubleClick={handleClick}>
           {props.val.val}
-        </Box>
+        </Typography>
       </Stack>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        <Box sx={{p: 1, width: getWidth()}}>
+          <Label val={editLabel()} sx={{fontSize: 12}}/>
+          <TextField 
+             fullWidth 
+             id="filled-basic" 
+             xlabel={'Edit the "' +  props.val.prop + '" property...'} 
+             variant="filled" 
+             defaultValue={props.val.val}
+             size="small"/>
+        </Box>
+      </Popover>
     </>
   );
 };
