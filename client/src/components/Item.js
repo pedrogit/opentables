82eaec47f011fedb35d28fbadd39c6ba707008e6
Pierre <pierre.racine@sbf.ulaval.ca>
2021-12-21 17:33:30 -0500

    Commented ou memo import

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,4 +1,5 @@
-import React, {memo} from "react";
+import React from "react";
+//import React, {memo} from "react";
 import JsxParser from "react-jsx-parser";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
