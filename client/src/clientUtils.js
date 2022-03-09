5591b8470f8a5a05a44668554f793cd49132693c
Pierre <pierre.racine@sbf.ulaval.ca>
2022-03-09 11:53:04 -0500

    Fixed token parsing broken in previous commit

--- client/src/clientUtils.js
+++ client/src/clientUtils.js
@@ -1,11 +1,10 @@
 import Cookies from 'js-cookie'
 const jwt = require("jsonwebtoken");
-const Globals = require("../../client/src/common/globals");
 
 const getUser = function() {
   var authtoken = Cookies.get('authtoken');
   if (authtoken) {
-    return jwt.decode(authtoken)[Globals.emailFieldName];
+    return jwt.decode(authtoken).email;
   }
   return '';
 }
