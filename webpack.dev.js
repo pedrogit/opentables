9df473022cd1eb1f0ce262cb81340a846cca453c
pedrogit <pierre.racine@sbf.ulaval.ca>
2022-06-09 15:21:37 -0400

    #39 - More eslint stuff

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,16 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const { merge } = require("webpack-merge");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   devServer: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:3001',
+      "/api": "http://localhost:3001",
+      secure: false,
     },
   },
-  devtool: 'inline-source-map',
-  plugins: [
-    new ESLintPlugin(),
-  ]
+  devtool: "inline-source-map",
+  plugins: [new ESLintPlugin()],
 });
