41b1e9c737ea55a6dd51ecb001c55dc039357b73
pedrogit <pierre.racine@sbf.ulaval.ca>
2022-06-13 14:39:51 -0400

    #39 - More cookie fixes

--- server/src/serverUtils.js
+++ server/src/serverUtils.js
@@ -5,6 +5,10 @@ exports.setCookieJWT = (req, res, payload) => {
   const jwtoken = jwt.sign(payload, process.env.AUTH_SECRET_TOKEN, {
     algorithm: "HS256",
   });
-  res.cookie("authtoken", jwtoken, { httpOnly: false, sameSite: "Lax" });
+  res.cookie("authtoken", jwtoken, {
+    httpOnly: false,
+    sameSite: "None",
+    secure: true,
+  });
   req.user = payload.username;
 };
