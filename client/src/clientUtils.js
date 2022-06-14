ecc5387322e336a60b04179b680726f20bcb18bd
pedrogit <pierre.racine@sbf.ulaval.ca>
2022-06-14 11:20:32 -0400

    #39 - Specify script domain when deleting cookie

--- client/src/clientUtils.js
+++ client/src/clientUtils.js
@@ -3,21 +3,6 @@ import Cookies from "js-cookie";
 const jwt = require("jsonwebtoken");
 const Globals = require("../../common/globals");
 
-const getUser = () => {
-  console.log(JSON.stringify(Cookies.get()));
-  const authtoken = Cookies.get("authtoken");
-  if (authtoken) {
-    const user = jwt.decode(authtoken).username;
-    // eslint-disable-next-line no-alert
-    console.log(`user=${user}`);
-    if (user !== null && user !== "") {
-      return user;
-    }
-    Cookies.remove("authtoken");
-  }
-  return Globals.allUserName;
-};
-
 const getURLParam = (paramName) => {
   const url = new URL(window.location.href);
   return url.searchParams.get(paramName);
@@ -29,4 +14,24 @@ const getScriptBaseURL = (() => {
   return () => thisScript.src.slice(0, -12);
 })();
 
-export { getUser, getURLParam, getScriptBaseURL };
+const getScriptDomain = () => {
+  // use URL constructor and return hostname
+  return new URL(getScriptBaseURL()).hostname;
+};
+
+const getUser = () => {
+  console.log(JSON.stringify(Cookies.get()));
+  const authtoken = Cookies.get("authtoken");
+  if (authtoken) {
+    const user = jwt.decode(authtoken).username;
+    // eslint-disable-next-line no-alert
+    console.log(`user=${user}`);
+    if (user !== null && user !== "") {
+      return user;
+    }
+    Cookies.remove("authtoken", { path: "", domain: `.${getScriptDomain()}` });
+  }
+  return Globals.allUserName;
+};
+
+export { getUser, getURLParam, getScriptBaseURL, getScriptDomain };
